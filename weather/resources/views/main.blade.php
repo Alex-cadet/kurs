0fb4d19777fa1805cd4eefa9625f872c29897c09
lex <14cadet83@gmail.com>
2021-11-08 22:02:45 +0300

    Кнопка

--- weather/resources/views/main.blade.php
+++ weather/resources/views/main.blade.php
@@ -9,5 +9,6 @@
 <body bgcolor="#6fd629">
     <h1 style="color:black;text-align:center">Курсовая работа</h1>
     <h2 style="color:cyan;text-align:center";">Погода</h2>
+    <button style="color:red;margin:auto;display:block;background-color:yellow;top:50%"">Открыть</button>
 </body>
 </html>
